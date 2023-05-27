9aa725b59784e47e1de4174f170585ad1babee20
Kushnir2205 <kushnirroman22@gmail.com>
2023-05-27 16:53:13 +0300

    fixed errors

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";s
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 
